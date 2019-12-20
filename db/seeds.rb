6cb085186d1ab9326f87d3a925413ed8d22d041c
Samantha Phipps <sammi.phipps@gmail.com>
2019-12-20 09:04:12 -0700

    added IncorrectAnswer.destroy_all to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+IncorrectAnswer.destroy_all
 CorrectAnswer.destroy_all
 Question.destroy_all
 Category.destroy_all
