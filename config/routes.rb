715fd114550fc91062d6953f1f065e85a383cba2
Samantha Phipps <sammi.phipps@gmail.com>
2020-02-10 18:39:51 -0700

    Noticed issue on the front end where categories were not being added correctly. After investigation, discovered that the routes for the category path were set for only index and show. Corrected route for this route but will also eventually need to do others.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:index, :show]
+  resources :categories, except: [:new, :edit]
   resources :questions, only: [:index, :show]
   resources :correct_answers, only: [:index, :show]
   resources :incorrect_answers, only: [:index, :show]
