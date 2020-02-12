38b30698f8c689155bb64933656f135e17ff2c67
Samantha Phipps <sammi.phipps@gmail.com>
2020-02-12 16:54:29 -0700

    Modified incorrect answer controller to include strong params

--- app/controllers/incorrect_answers_controller.rb
+++ app/controllers/incorrect_answers_controller.rb
@@ -10,19 +10,13 @@ class IncorrectAnswersController < ApplicationController
     end 
 
     def create 
-        incorrect_answer = IncorrectAnswer.create(
-            message: params[:message],
-            question_id: params[:question_id]
-        )
+        incorrect_answer = IncorrectAnswer.create(incorrect_answer_params)
         render json: incorrect_answer, include: [:question => {only: [:id, :message]}], except: [:created_at, :updated_at]
     end 
 
     def update 
         incorrect_answer = IncorrectAnswer.find(params[:id])
-        incorrect_answer.update(
-            message: params[:message],
-            question_id: params[:question_id]
-        )
+        incorrect_answer.update(incorrect_answer_params)
         render json: incorrect_answer, include: [:question => {only: [:id, :message]}], except: [:created_at, :updated_at]
     end 
 
@@ -30,5 +24,11 @@ class IncorrectAnswersController < ApplicationController
         incorrect_answer = IncorrectAnswer.find(params[:id])
         incorrect_answer.destroy
         render json: {message:"This answer has been destroyed." , status: 204}
+    end 
+
+    private
+
+    def incorrect_answer_params
+        params.require(:incorrect_answer).permit(:message, :question_id)
     end 
 end
