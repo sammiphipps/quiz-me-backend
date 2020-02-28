bde465ad2499988b1330df211488632ade64f9ef
Samantha Phipps <sammi.phipps@gmail.com>
2020-02-27 17:01:19 -0700

    Noticed on the front end that when a category with a question attached is removed that it didn't transfer to the backend. In order to fix this, I added a dependent destroy to category and question.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :category
-  has_one :correct_answer
-  has_many :incorrect_answers
+  has_one :correct_answer, dependent: :destroy
+  has_many :incorrect_answers, dependent: :destroy
 end
