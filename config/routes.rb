bcbc4357a75f702a8f37b83d0384739128e65488
Samantha Phipps <sammi.phipps@gmail.com>
2021-01-30 14:43:39 -0500

    Add root to routes.rb to attempt to fix heroku issue.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "application#index"
   resources :categories, except: [:new, :edit]
   resources :questions, except: [:new, :edit]
   resources :correct_answers, except: [:new, :edit]
